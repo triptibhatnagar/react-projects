9f3e9fcd11104916179cc9b2147d460009d181f7
triptibhatnagar <bhatnagartripti2727@gmail.com>
2025-11-16 01:09:55 +0530

    custom react

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,13 @@
 import Chai from "./Chai"
 
 function App() {
+  const username = "hiteshhhh"
+  // write js evaluation here
   return (
     <>
-      <h1>Chai aur react wth vite</h1>
+      <h1>Chai aur react wth vite {username}</h1>
+      {/* {} -> evaluated expression (we write final evaluated outcome of js, not the js)*/}
+      {/* means u can't write {if(true) username} */}
       <Chai/>
     </>
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,6 +1,66 @@
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-    <App />
+function MyApp() {
+    // VARIABLES
+    const username = "chai aur code"
+    return (
+        <div>
+            <h1>Custom App | {username}</h1>
+            {/* {} -> Evaluated expression */}
+        </div>
+    )
+}
+
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href: "https://google.com",
+//         target: "_blank"
+//     },
+//     children: 'Click me to visit google'
+// }
+
+const anotherElement = (
+    <a href="https://google.com" target="_blank">Visit Gooogle</a>
 )
+
+const anotherUser = "chai aur react"
+// how to inject variables in this
+// TREE FORMATION -> THEN VARIABLE INJECTIONS COME
+// so anotherUser will automatically go inside ReactElem
+
+
+// so creating element acc to react
+// REACT RENDER
+const ReactElem = React.createElement(
+    'a',
+    {href: "https://google.com", target: "_blank"},
+    "Click me to visit google",
+    anotherUser
+    // can't write if, else etc here
+    // evaluated expressiona written here as they as it is go to browser
+)
+
+createRoot(document.getElementById('root')).render(
+    // <MyApp />
+    // MyApp()
+    // < ReactElement />
+    // But function expected, not object
+    // ReactElement // did not worked due to syntax
+    // anotherElement // worked
+    ReactElem // worked
+    // <App/>
+    // <MyApp/>
+)
+
+// If MyApp is a function Then how it's written in JSX
+// Converting syntaxes is work of bundler
+// html syntax -> parsed to -> object
+
+
+// If returned HTML is converted to the object then why not to pass object directly
+
+
+// The object did not work but the HTML inside another object worked So it is because maybe the syntax that we are writing inside the object, the key value pairs are not according to the syntax
